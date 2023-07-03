9aef5aeaea5c59540790b3efdcf4067e648caf9b
Stanislav Hristov <101987485+StanislavHristov@users.noreply.github.com>
2023-07-03 23:06:48 +0300

    Update Lab6.sql

--- LabExercises/Lab6/Lab6.sql
+++ LabExercises/Lab6/Lab6.sql
@@ -3,46 +3,46 @@ CREATE DATABASE school_sport_clubs;
 USE school_sport_clubs;
 
 CREATE TABLE school_sport_clubs.sports(
-	id INT AUTO_INCREMENT PRIMARY KEY ,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE school_sport_clubs.coaches(
-	id INT AUTO_INCREMENT PRIMARY KEY ,
-	name VARCHAR(255) NOT NULL ,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
 	egn VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE school_sport_clubs.students(
-	id INT AUTO_INCREMENT PRIMARY KEY ,
-	name VARCHAR(255) NOT NULL ,
-	egn VARCHAR(10) NOT NULL UNIQUE ,
-	address VARCHAR(255) NOT NULL ,
-	phone VARCHAR(20) NULL DEFAULT NULL ,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	egn VARCHAR(10) NOT NULL UNIQUE,
+	address VARCHAR(255) NOT NULL,
+	phone VARCHAR(20) NULL DEFAULT NULL,
 	class VARCHAR(10) NULL DEFAULT NULL   
 );
 
 CREATE TABLE school_sport_clubs.sportGroups(
-	id INT AUTO_INCREMENT PRIMARY KEY ,
-	location VARCHAR(255) NOT NULL ,
-	dayOfWeek ENUM('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') ,
-	hourOfTraining TIME NOT NULL ,
-	sport_id INT NULL ,
-	coach_id INT NULL ,
-	UNIQUE KEY(location,dayOfWeek,hourOfTraining)  ,
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	location VARCHAR(255) NOT NULL,
+	dayOfWeek ENUM('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'),
+	hourOfTraining TIME NOT NULL,
+	sport_id INT NULL,
+	coach_id INT NULL,
+	UNIQUE KEY(location,dayOfWeek,hourOfTraining),
 	CONSTRAINT FOREIGN KEY(sport_id) 
-		REFERENCES sports(id) ,
+		REFERENCES sports(id),
 	CONSTRAINT FOREIGN KEY (coach_id) 
 		REFERENCES coaches(id)
 );
 
 CREATE TABLE school_sport_clubs.student_sport(
-	student_id INT NOT NULL , 
-	sportGroup_id INT NOT NULL ,  
+	student_id INT NOT NULL, 
+	sportGroup_id INT NOT NULL,  
 	CONSTRAINT FOREIGN KEY (student_id) 
-		REFERENCES students(id) ,	
+		REFERENCES students(id),	
 	CONSTRAINT FOREIGN KEY (sportGroup_id) 
-		REFERENCES sportGroups(id) ,
+		REFERENCES sportGroups(id),
 	PRIMARY KEY(student_id,sportGroup_id)
 );
 
@@ -53,7 +53,7 @@ CREATE TABLE taxesPayments(
 	paymentAmount DOUBLE NOT NULL,
 	month TINYINT,
 	year YEAR,
-	dateOfPayment DATETIME NOT NULL ,
+	dateOfPayment DATETIME NOT NULL,
 	CONSTRAINT FOREIGN KEY (student_id) 
 		REFERENCES students(id),
 	CONSTRAINT FOREIGN KEY (group_id) 
